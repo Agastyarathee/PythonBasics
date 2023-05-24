93f6187f61d8e1b34e6d40ad5b7f2164023936f4
Agastyarathee <67199509+Agastyarathee@users.noreply.github.com>
2023-05-24 18:58:20 +0530

    roll-a-dice

---
+++ RollADice.py
@@ -0,0 +1,77 @@
+import random
+
+v="y"
+
+while v=="y":
+    
+    v=input("Press y be happy")
+    num=random.randint(1,6)
+    
+    if num ==1:
+        print('''
+        [   ]
+        [   ]
+        [ 0 ]
+        [   ]
+        [   ]
+        
+              
+              ''')
+        
+    elif num ==2:
+        print('''
+        [   ]
+        [ 0 ]
+        [   ]
+        [ 0 ]
+        [   ]
+        
+              
+              ''')
+    elif num ==3:
+        print('''
+        [   ]
+        [ 0 ]
+        [ 0 ]
+        [ 0 ]
+        [   ]
+        
+              
+              ''')
+    elif num ==4:
+        print('''
+        [   ]
+        [0 0]
+        [   ]
+        [0 0]
+        [   ]
+        
+              
+              ''')
+    elif num ==5:
+        print('''
+        [   ]
+        [0 0]
+        [ 0 ]
+        [0 0]
+        [   ]
+        
+              
+              ''')
+    elif num ==6:
+        print('''
+        [   ]
+        [0 0]
+        [0 0]
+        [0 0]
+        [   ]
+        
+              
+              ''')
+        
+    
+
+        
+
+
+
